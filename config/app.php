c91fb8ea261342395653b4e832cfc73e9997a4fd
name <mail>
2015-11-14 17:35:07 +0900

    Install Plugin Setting
    https://github.com/barryvdh/laravel-ide-helper
    https://github.com/barryvdh/laravel-debugbar
    https://github.com/LaravelCollective/html

--- config/app.php
+++ config/app.php
@@ -145,6 +145,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -194,6 +197,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
